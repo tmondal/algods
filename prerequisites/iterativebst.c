6837a9348a7e9d069b72bac23c2425726e56e95e
Tanmoy <tanmoy94m@gmail.com>
2017-09-07 19:33:43 +0530

    gitignore added

--- prerequisites/iterativebst.c
+++ prerequisites/iterativebst.c
@@ -9,8 +9,23 @@ typedef struct Bst
 	Bst *right;
 }Bst;
 
+Bst	*createNode(int data){
+	Bst *newnode = (Bst *)malloc(sizeof(Bst));
+	newnode->data = data;
+	newnode->left = newnode->right = NULL;
+	return newnode;
+}
+
+void insert(Bst **root, int data){
+	
+
+	Bst *temp = createNode(data);
+	
+}
+
 int main(int argc, char const *argv[])
 {
-	Bst *root = NULL;	
+	Bst *root = NULL;
+	insert(&root,5);	
 	return 0;
 }
