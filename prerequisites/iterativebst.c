b3153ca7a79d85d2df27613c34cd205d41abd51b
Tanmoy <tanmoy94m@gmail.com>
2017-09-07 19:27:29 +0530

    again merge conflict

---
+++ prerequisites/iterativebst.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Bst Bst;
+typedef struct Bst
+{
+	int data;
+	Bst *left;
+	Bst *right;
+}Bst;
+
+int main(int argc, char const *argv[])
+{
+	Bst *root = NULL;	
+	return 0;
+}
